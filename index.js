d1915147a4f1667a66f31cf7010a441e89cee291
Stevie <stevie.leveque@cgi.com>
2022-04-11 14:58:36 +0200

    ajout input

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 try {
-    console.log("Hello")
+    console.log("Hello " + core.getInput("name"))
     console.log(github)
 } catch (error) {
     core.setFailed(error.message)
